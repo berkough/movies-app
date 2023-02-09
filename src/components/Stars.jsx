54f73eda927b1593ed883ca27ee51d5a55be5ceb
Mathew Kane <berkough@gmail.com>
2023-02-09 12:55:04 -0800

    almost have the stars working

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaStar} from 'react-icons/fa'
 
 export default function Stars(){
+  const [rating, setRating]= useState(null);
+  const [hover, setHover] = useState(null);
   return (
     <div>
-      {[...Array(5)].map((start)=>{
-        return <FaStar size={50}/>
+      {[...Array(5)].map((star, i)=>{
+        const ratingValue = i + 1;
+        
+        return (
+          <label 
+          type='radio' 
+          name='rating' 
+          value={ratingValue} 
+          onClick={()=> setRating(ratingValue)}
+          >
+            <FaStar 
+            className='star' 
+            color={(hover || ratingValue) <= rating ? "#ffc107" : "#efe5e9"} 
+            size={50}
+            onMouseEnter={()=> setHover(ratingValue)}
+            onMouseLeave={()=> setHover(null)}
+            />
+          </label>
+        )
       })}
-      
+      <p>The rating is {rating}.</p>
     </div>
   )
 }
